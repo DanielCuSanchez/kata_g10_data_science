c2904cf5d61c164f6cd0865d002b53bb76c407ae
Daniel Cu Sánchez <danielcu@alternet.com.mx>
2022-07-26 20:37:22 -0500

    python

---
+++ numpy/basic/index.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+myArray = np.array([[[1,1,2,3],[223,44,2,3]],[[156,1985,22,3],[458,64,32,30]]])
+
+# print(myArray.ndim)
+# print(myArray[0][1])
+
+print(np.sort(myArray))
+print(myArray.shape)
+
+
+
+#print(type(myArray))
+
+#print(np.__doc__)
---
+++ numpy/random/random.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+
+number_random = np.random.randint(100, size=10)
+
+print(number_random)
